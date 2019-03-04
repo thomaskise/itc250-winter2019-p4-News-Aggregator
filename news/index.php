9b5679ff9fd8cd793b2e20a30bf128c7c9734b76
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-03-04 11:11:13 -0800

    Revert "Derrick"

--- news/index.php
+++ news/index.php
@@ -9,6 +9,7 @@
  * @license https://www.apache.org/licenses/LICENSE-2.0
  * @todo none
  */
+
 # '../' works for a sub-folder.  use './' for the root  
 require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials 
  
@@ -16,21 +17,26 @@ require '../inc_0700/config_inc.php'; #provides configuration, pathing, error ha
 // change line 20 for topics
 //$sql = "select * from wn19_<xxx>
 //Placeholder SQL to make page work
-$sql = "Select * From wn19_FeedCategories";
 
-$sqlTopics = "SELECT * FROM `wn19_Topics`";
+$sql = "Select * From wn19_FeedCategories";
 /*$sql = "
 select CONCAT(a.FirstName, ' ', a.LastName) AdminName, s.SurveyID, s.Title, s.Description, 
 date_format(s.DateAdded, '%W %D %M %Y %H:%i') 'DateAdded' from "
 . PREFIX . "surveys s, " . PREFIX . "Admin a where s.AdminID=a.AdminID order by s.DateAdded desc
 ";*/
+
+
+
 #Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
 $config->titleTag = '<RSS Feed Categories>';
+
 #Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
 $config->metaDescription = 'Seattle Central\'s ITC250 Class news are made with pure PHP! ' . $config->metaDescription;
 $config->metaKeywords = 'news,PHP,Fun,Bran,Regular,Regular Expressions,'. $config->metaKeywords;
+
 //adds font awesome icons for arrows on pager
 $config->loadhead .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
+
 /*
 $config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
 $config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
@@ -43,7 +49,9 @@ $config->sidebar2 = ''; #goes inside right side of page
 $config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewable this page only)!!
 $config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
 */
+
 # END CONFIG AREA ---------------------------------------------------------- 
+
 get_header(); #defaults to theme header or header_inc.php
 ?>
 <h3 align="center">Category List</h3>
@@ -59,21 +67,25 @@ get_header(); #defaults to theme header or header_inc.php
 #reference images for pager
 //$prev = '<img src="' . $config->virtual_path . '/images/arrow_prev.gif" border="0" />';
 //$next = '<img src="' . $config->virtual_path . '/images/arrow_next.gif" border="0" />';
+
 #images in this case are from font awesome
 $prev = '<i class="fa fa-chevron-circle-left"></i>';
 $next = '<i class="fa fa-chevron-circle-right"></i>';
+
 # Create instance of new 'pager' class
 $myPager = new Pager(5,'',$prev,$next,'');
 $sql = $myPager->loadSQL($sql);  #load SQL, add offset
+
 # connection comes first in mysqli (improved) function
 $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+
 if (isset($_GET['pg'])) {
     $_SESSION['currentpage'] = $_GET['pg'];
 }else{
     $_SESSION['currentpage'] = 1;
 }
 // update the followiong for news
-/*
 if(mysqli_num_rows($result) > 0)
 {#records exist - process
 	if($myPager->showTotal()==1){
@@ -97,20 +109,6 @@ if(mysqli_num_rows($result) > 0)
                   <tr>
                   <td><a href="' . VIRTUAL_PATH . 'news/topic_view.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['CategoryName']) . '</a></td>
                   </tr>';
-                echo '<ul class="side-nav-second-level" aria-expanded="false">';
-    
-    			$resultTopics = mysqli_query(IDB::conn(),$sqlTopics) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
-
-    			if(mysqli_num_rows($resultTopics) > 0){
-      				while($roww = mysqli_fetch_assoc($resultTopics)){
-        				if($row['CategoryID'] == $roww['CategoryID']){
-          					echo '<li>';
-    						echo '<a href="list.php?feedid='.$roww['CategoryID'].'">'.$roww['TopicName'].'</a>';
-          					echo '</li>';
-        				}
-      				}      
-    			}
-    echo '</ul>';
               }
               echo '</tbody>
             </table>
@@ -119,46 +117,7 @@ if(mysqli_num_rows($result) > 0)
 }else{#no records
    echo "<div align=center>There are currently no surveys!</div>";	
 }
-*/
-?>
-
-<ul class="metismenu side-nav side-nav-light">
-
-<?php
-if(mysqli_num_rows($result) > 0){
-  if($myPager->showTotal()==1){
-        $itemz = "category";}
-    else{$itemz = "categories";}  //deal with plural
-    # Output the number of surveys
-    echo '<h4>We have ' . $myPager->showTotal() . ' ' . $itemz . ' as listed:</h4>';
-  while($row = mysqli_fetch_assoc($result)){
-    echo '<li class="side-nav-item">';
-    echo '<a href="javascript: void(0);" class="side-nav-link">';
-    echo '<i class="dripicons-copy"></i>';
-    echo '<span> '. $row['CategoryName'].'</span> ';
-    echo '<span class="menu-arrow"></span>';
-    echo '</a>';
-    echo '<ul class="side-nav-second-level" aria-expanded="false">';
-    
-    $resultTopics = mysqli_query(IDB::conn(),$sqlTopics) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
-
-    			if(mysqli_num_rows($resultTopics) > 0){
-      				while($roww = mysqli_fetch_assoc($resultTopics)){
-        				if($row['CategoryID'] == $roww['CategoryID']){
-          					echo '<li>';
-    						echo '<a href="topic_view.php?feedid='.$roww['TopicID'].'">'.$roww['TopicName'].'</a>';
-          					echo '</li>';
-        				}
-      				}      
-    			}
-    echo '</ul>';
-    echo '</li>';
-  }
-}
-
-
-
-
 @mysqli_free_result($result);
+
 get_footer(); #defaults to theme footer or footer_inc.php
 ?>

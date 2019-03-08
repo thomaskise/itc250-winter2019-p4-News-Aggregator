a4f17e188cb78b5f1db261748d5ede22b664e038
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-03-08 00:48:05 -0800

    updated sessions - still wip

--- news/inc_news/sessions_news.php
+++ news/inc_news/sessions_news.php
@@ -21,83 +21,79 @@
  * @license https://www.apache.org/licenses/LICENSE-2.0
  * @todo none
  */
-    //lines 17-19 will be removed as these are redundant in the parent module
+    //lines 25-35 will be removed as these are redundant in the parent module
     require '../../inc_0700/config_inc.php'; 
     $url = 'https://news.google.com/rss/search?q=vegetarian+foods&hl=en-US&gl=US&ceid=US:en';
+//    $url = 'https://news.google.com/rss/search?q=vegan+food&hl=en-US&gl=US&ceid=US:en';
+//    $url = 'https://news.google.com/rss/search?q=thai+food&hl=en-US&gl=US&ceid=US:en';
+//    $url = 'https://news.google.com/rss/search?q=modern+art&hl=en-US&gl=US&ceid=US:en'; 
+//    $url = 'https://news.google.com/rss/search?q=impressionism&hl=en-US&gl=US&ceid=US:en'; 
+//    $url = 'https://news.google.com/rss/search?q=ancient+greek+art&hl=en-US&gl=US&ceid=US:en'; 
+//    $url = 'https://news.google.com/rss/search?q=travel+grand+canyon&hl=en-US&gl=US&ceid=US:en'; 
+//    $url = 'https://news.google.com/rss/search?q=travel+disneyland&hl=en-US&gl=US&ceid=US:en'; 
+//    $url = 'https://news.google.com/rss/search?q=travel+london&hl=en-US&gl=US&ceid=US:en'; 
     $topid = '1';
-	$file = file_get_contents($url);
-    $result = new SimpleXMLElement($file);
 
-	//$result = new SimpleXMLElement($file);
-    //echo $file;
-    //echo $result->channel->item;
-    //die();
-    startSession();
-    
     $nowtime=time();
+    $maxSession=600;
+    $secondsSinceRefresh = 0;
+    $file = '';
+    $fileSource = '';
+    $sessionFoundAlive = 'no';
 
-// if the session is not set then set it
-// if it is set, then check for the topic
-// if the topic is found then check to see if it has expired
-// if any of the above fail, retrieve the rss feed from the internet
-// load the session data in either $file or $result (tbd)
+    if(!isset($_SESSION)){// if the session isn't set start the session and intitalize as array
+        echo 'session not set <br />';
+        startSession();
+        $_SESSION['NewsFeeds'] = array(); //think feeds - if we don't have an array, start one
+    }//end if
+    echo '<pre>';
+        echo var_dump($_SESSION['NewsFeeds']);
+    echo '</pre>';
+    foreach ($_SESSION['NewsFeeds'] as $feed) {//loop through sessions to find the right one if it exists and is not expired
+        echo 'Here\'s the topic id I found: ' . $feed->TopicID . '<br />';
+        if($feed->TopicID == $topid) {// find the topicID
+            $secondsSinceRefresh == int(time() - int($feed->SessionStartTime) + 10);//add 10 seconds to allow time for retrieval
+            if($maxSession >  $secondsSinceRefresh + 10) {//use the session if it is fresh (10 second jic is added)
+                $fileSource = 'session found & not expired';
+                $sessionFoundAlive = 'yes';
+                $file = $feed->RssFeed;
+            }//end if not timed out
+         }//end if topicID
+    }//end foreach
 
-//    if(!isset($_SESSION['NewsFeeds'])){
-        $_SESSION["NewsFeeds"] = array();        
-        $cacheStartTime=$nowtime;
-        $myItem = new NewsFeed(session_id(), $topid, $cacheStartTime);
-        $myItem->rssFeed($result);
-        $_SESSION["NewsFeeds"] = array($myItem);
-
-        echo '<b><u>This is the result of a foreach statement: </u></b><br />';
-        foreach ($_SESSION["NewsFeeds"] as $key => $value) {
-            echo 'My key is: ' . $key . '<br />';
-            echo 'My value is: ' . $value . '<br /><br />';
-        }
-
-        echo '<b><u>This is a simple echo of a variable: </u></b><br />';
-        echo $cacheStartTime . '<br />';
-        echo '<br />';
-        //echo 'This is a var_dump of $myItem: <br /><br />';
-            //var_dump($myItem);
-            //var_dump($_SESSION['NewsFeeds']['$myItem->SessionID']);
-        //$currentSessionID=($_SESSION["NewsFeed"][0]["SessionID"]);
-        //echo $currentSessionID;
-
-        echo '<b><u>This is an echo($_SESSION)) : </u></b><br />';
-        echo($_SESSION["NewsFeeds"]["Array"]) . '<br /><br />';
+    if($sessionFoundAlive == 'no'){ //if the session isn't found alive, then set it
+        echo 'sesssion null or not found, so start <br /><br />';
+        $fileSource = 'session not found';
+        $file = file_get_contents($url);// get the rss feed from the internet
+        $_SESSION['NewsFeeds'][] = new NewsFeed($topid, time(), $file);
+        $secondsSinceRefresh = time() - time();
+    }
+        //dumpDie($_SESSION['NewsFeeds']);
+        echo 'Results: <br />';
+        echo 'was session found alive? ' . $sessionFoundAlive . ' <br /><br />';
+        echo 'max session: ' . $maxSession . ' <br /><br />';
+        echo 'seconds since refresh: ' . $secondsSinceRefresh . '<br /><br />';
+        echo 'File came from what loop?  ' . $fileSource . '<br /><br />';
+        echo 'file data: ' . $file . '<br />';
 
         echo '<pre>';
             echo '<b><u>This is a print_r($_SESSION)) : </u></b><br />';
             print_r($_SESSION) . '<br />';
             //echo 'session id = ' . $_SESSION["NewsFeeds"]["SessionID"];
         echo '</pre>';
-
         echo '<b><u>This is marginal success</u></b>';
-        die();
-        
- //   }elseif($_SESSION['NewsFeeds']
-//      ){
-       //  $_SESSION['test'][] = new SimpleXMLElement($file);
-        //if($_SESSION['sessionid'][])
-//    }
+
     
 class NewsFeed{
-    public $SessionID = '';
     public $TopicID ='';
-    public $SessionTime = '';
-    public $feedArray = array();
+    public $SessionStartTime = '';
+    public $RssFeed = '';
     
-    public function __construct($SessionID, $TopicID, $SessionTime){
-        $this->SessionID = $SessionID;
+    public function __construct($TopicID, $SessionStartTime, $RssFeed){
         $this->TopicID = $TopicID;
-        $this->SessionTime = $SessionTime;
+        $this->SessionStartTime = $SessionStartTime;
+        $this->RssFeed = $RssFeed;
     }//end NewsFeed constructor
-    
-    public function rssFeed($rss) {
-        $this->Feeds = $rss;
-        
-    }//end rssFeed
 }//end NewsFeed class
 
 ?>

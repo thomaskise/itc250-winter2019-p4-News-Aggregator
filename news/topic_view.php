fa320132ccaeba060b6adf912876d7489415fd65
ElizaJ <eljones.millson@gmail.com>
2019-03-13 16:06:19 -0700

    adds docblock to topic_view

--- news/topic_view.php
+++ news/topic_view.php
@@ -1,63 +1,82 @@
 <?php
 /**
  *************************************************************************
+ *  topic_view.php
  *************************************************************************
  *
- *  needs to be updated for topic_view.php
+ * topic_view.php displays a list of News Topics once the user has chosen a Category
  *
- *************************************************************************
- *************************************************************************
- *
- * survey_view.php is a page to demonstrate the proof of concept of the 
- * initial SurveySez objects.
- *
- * Objects in this version are the Survey, Question & Answer objects
- * 
- * @package SurveySez
- * @author William Newman
- * @version 2.12 2015/06/04
- * @link http://newmanix.com/ 
+ * @package Cottlets website
+ * @subpackage News feed
+ * @author itc250 winter 2019 project team
+ * @author Junqiao Mou(Derrick)
+ * @author Elizabeth Jones
+ * @author Thom Harrington
+ * @author Fikirte Mulugeta
+ * @version 2.0 March 12, 2019
+ * @version 1.0 2019/03/05
+ * @link https://d.zjwda.org/news/
+ * @link http//elizajcreates.com/itc250/wn19/news/
+ * @link https://kiseharrington.com/wn19/news/
  * @license http://www.apache.org/licenses/LICENSE-2.0
- * @see Question.php
- * @see Answer.php
- * @see Response.php
- * @see Choice.php
+ * @see news/index.php
+ * @todo none
  */
  
 require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
 /*spl_autoload_register('MyAutoLoader::NamespaceLoader');//required to load SurveySez namespace objects*/
 $config->metaRobots = 'no index, no follow';#never index survey pages
 
-# check variable of item passed in - if invalid data, forcibly redirect back to demo_list.php page
+# check variable of item passed in - if invalid data, forcibly redirect back to index.php page
 if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystring
 	 $myID = (int)$_GET['id']; #Convert to integer, will equate to zero if fails
 }else{
 	myRedirect(VIRTUAL_PATH . "news/index.php");
 }
 
+#SQL to get the Topic names and urls from the DB
+
 $sql = "Select * From wn19_Topics Where CategoryID= " . $_GET['id'];
 
-/*$mySurvey = new SurveySez\Survey($myID); //MY_Survey extends survey class so methods can be added
-if($mySurvey->isValid)
-{
-	$config->titleTag = "'" . $mySurvey->Title . "' Survey!";
-}else{
-	$config->titleTag = smartTitle(); //use constant 
-}*/
+//adds font awesome icons for arrows on pager
+$config->loadhead .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
+
 #END CONFIG AREA ---------------------------------------------------------- 
 
 get_header(); #defaults to theme header or header_inc.php
 ?>
 <h3>Topics</h3>
 
+<p>What Topic are you interested in learning more about today?.</p>
+<p>To see the related RSS Feed please click one of the links below.</p><BR />
+
 <?php
+
+#images in this case are from font awesome
+$prev = '<i class="fa fa-chevron-circle-left"></i>';
+$next = '<i class="fa fa-chevron-circle-right"></i>';
+
+# Create instance of new 'pager' class
+$myPager = new Pager(5,'',$prev,$next,'');
+$sql = $myPager->loadSQL($sql);  #load SQL, add offset
 
 $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
+if (isset($_GET['pg'])) {
+    $_SESSION['currentpage'] = $_GET['pg'];
+}else{
+    $_SESSION['currentpage'] = 1;
+}
 
 if(mysqli_num_rows($result) > 0)
 {#records exist - process
+    if($myPager->showTotal()==1){
+        $itemz = "topic";}
+    else{$itemz = "topics";}  //deal with plural
 	
+      # Output the number of categories
+    echo '<h4>We have ' . $myPager->showTotal() . ' ' . $itemz . ' as listed:</h4>';
+    
     # Create the table header
     echo '
         <div class="table-responsive">
@@ -78,23 +97,14 @@ if(mysqli_num_rows($result) > 0)
               echo '</tbody>
             </table>
         </div>'
-        /*. $myPager->showNAV()*/; # show paging nav, only if enough records	 
+        . $myPager->showNAV(); # show paging nav, only if enough records	 
 }else{#no records
    echo "<div align=center>There are currently no topics!</div>";	
 }
+
 @mysqli_free_result($result);
 
-
-
-/*if($mySurvey->isValid)
-{ #check to see if we have a valid SurveyID
-	echo '<p>' . $mySurvey->Description . '</p>';
-	echo $mySurvey->showQuestions();
-}else{
-	echo "The survey detail has not yet been created!";	
-}
-
- echo '<a href="' . VIRTUAL_PATH . 'surveys/index.php?pg=' . $_SESSION["currentpage"] . '">Check out another survey?</a>';*/
+ echo '<a href="' . VIRTUAL_PATH . 'news/index.php?pg=' . $_SESSION["currentpage"] . '">Check out another Category?</a>';
 
 get_footer(); #defaults to theme footer or footer_inc.php
 
